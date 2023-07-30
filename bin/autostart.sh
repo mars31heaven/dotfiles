b0c082ffdc2cdec55664c80e9601b8b54ed6703c
mars31heaven <marcelodsa@tutanota.com>
2023-07-30 18:46:32 -0300

    Script to launch applications with Wayland session

---
+++ bin/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+swayidle -w \
+	timeout 300 'hyprctl dispatch dpms off' \
+	resume 'hyprctl dispatch dpms on' &
+
+wlsunset -t 4500 -T 6500 -l -1 -L -1 -g 1.0 &
+
+wl-paste -t text --watch clipman store --no-persist &
+
+ironbar &
+
+dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
+
+mako
