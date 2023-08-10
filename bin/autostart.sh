09cd4309198a819a7fdd5be1f36abf5ee105fdeb
mars31heaven <marcelodsa@tutanota.com>
2023-08-10 18:10:29 -0300

    Small changes to custom scripts

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -8,6 +8,8 @@ wlsunset -t 4500 -T 6500 -l -1 -L -1 -g 1.0 &
 
 wl-paste -t text --watch clipman store --no-persist &
 
+swaybg --color '#282828' &
+
 ironbar &
 
 dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
