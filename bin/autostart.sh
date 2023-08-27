6bcb13d27dbb630d059b079c6e59316551d1ace4
mars31heaven <marcelodsa@tutanota.com>
2023-08-27 14:54:15 -0300

    Switched to pipewire

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -4,14 +4,16 @@ swayidle -w \
 	timeout 300 'hyprctl dispatch dpms off' \
 	resume 'hyprctl dispatch dpms on' &
 
-wlsunset -t 4500 -T 6500 -l -1 -L -1 -g 1.0 &
-
 wl-paste -t text --watch clipman store --no-persist &
 
-swaybg --color '#282828' &
+swaybg --color '#1c1c1c' &
 
 ironbar &
 
 dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
+
+/usr/bin/pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/wireplumber &
 
 mako
